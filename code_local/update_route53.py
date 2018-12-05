889262b729f0ed19890a523557e6225143403150
Kareem Straker <kareem.straker@gmail.com>
2018-12-05 17:15:40 +0100

    updated python local code

--- code_local/update_route53.py
+++ code_local/update_route53.py
@@ -42,7 +42,7 @@ def parse_args():
         # specify argument
         "--aws_domain",
         # what variable argument will be written to
-        dest="domain",
+        dest="AWS_DOMAIN",
         # text that is returned when using --help
         help="[REQUIRED] route53 zone",
         # default value if arg not specified by user
@@ -60,63 +60,70 @@ def parse_args():
     )
     parser.add_argument(
         "--aws_key",
-        dest="aws_access_key_id",
+        dest="AWS_ACCESS_KEY_ID",
         help="[REQUIRED] AWS access key ID"
     )
     parser.add_argument(
         "--aws_secret",
-        dest="aws_secret_access_key",
+        dest="AWS_SECRET_ACCESS_KEY",
         help="[REQUIRED] AWS secret access key"
     )
     parser.add_argument(
         "--aws_region",
-        dest="aws_region_name",
+        dest="AWS_REGION_NAME",
         help="[OPTIONAL] AWS region name"
     )
     parser.add_argument(
         "--aws_stream_name",
-        dest="aws_stream_name",
+        dest="AWS_STREAM_NAME",
         help="[OPTIONAL] AWS kinesis stream name"
     )
     parser.add_argument(
         "--aws_partition_key",
-        dest="aws_partition_key",
+        dest="AWS_PARTITION_KEY",
         help="[OPTIONAL] AWS kinesis stream partition key"
     )
     return parser.parse_args()
 
 
-def check_domain(domain):
+def check_element(element_name, element_var=None,):
     """
-    Function to check DOMAIN is present and valid
+    Function designed to check for variable elements for a variety of sources,
+    and return that. Checks variable element_name and element_var.
+    Example elements:
+    aws_domain, aws_access_key_id, aws_secret_access_key,
+    aws_region_name, aws_stream_name, aws_partition_key
+
+    Function returns single checked value.
     """
 
-    # check if key is present as input var
-    if domain:
-        print "AWS_DOMAIN variable from arg/flag found!"
-        return domain
+    # print debug element name and variable
+    # print "element_name: {}".format(element_name)
+    # print "element_var: {}".format(element_var)
 
-    # check if key is present as env var
-    if os.environ.get("AWS_DOMAIN"):
-        print "AWS_DOMAIN variable from ENV found!"
-        return os.environ["AWS_DOMAIN"]
+    # check if element_var is present as input var
+    if element_var:
+        print "{} found from arg/flag!".format(element_name)
+        return element_var
 
-    # check if key is present as settings_file
+    # check if element_var is present as ENV var
+    if os.environ.get("{}".format(element_name)):
+        print "{} found from ENV variable!".format(element_name)
+        return os.environ["{}".format(element_name)]
+
+    # check if element_var is present as settings_file
     try:
-        if settings_file.AWS_DOMAIN:
-            print "AWS_DOMAIN variable from settings_file found!"
-            return settings_file.AWS_DOMAIN
+        if getattr(settings_file, element_name, None):
+            print "{} found from settings_file!".format(element_name)
+            return getattr(settings_file, element_name, None)
     except Exception:
         pass
 
-    # raise exception if domain variable is still nothing
-    if not domain or domain is None or domain == "None":
-        raise Exception("AWS_DOMAIN not found or specified!")
+    # raise exception if element_var variable is still nothing
+    if not element_var or element_var is None or element_var == "None":
+        raise Exception("{} not found or specified!".format(element_name))
 
-    # debug print domain
-    # print domain
-
-    return domain
+    return element_var
 
 
 def check_ip(check_url, ip=None):
@@ -124,7 +131,7 @@ def check_ip(check_url, ip=None):
     Function to check IP is present and valid
     """
     # debug print url
-    print "Request URL: {}".format(check_url)
+    print "IP requested from URL: {}".format(check_url)
 
     # check if ip is a prescribed variable, or is empty, and go get
     if not ip or ip == "None":
@@ -149,108 +156,25 @@ def check_ip(check_url, ip=None):
     return ip
 
 
-def check_aws_creds(aws_access_key_id, aws_secret_access_key, aws_region_name):
-    """
-    Function to check AWS Credentials are present and valid
-    """
-
-    # check if key is present as input var
-    if aws_access_key_id and aws_secret_access_key and aws_region_name:
-        print "aws_access_key_id variable from arg/flag found!"
-        print "aws_secret_access_key variable from arg/flag found!"
-        print "aws_region variable from arg/flag found!"
-        return aws_access_key_id, aws_secret_access_key, aws_region_name
-
-    # check if key is present as env var
-    if (os.environ.get("AWS_KEY") and
-            os.environ.get("AWS_SECRET") and
-            os.environ.get("AWS_KEY")):
-        print "AWS_KEY variable from ENV found!"
-        print "AWS_SECRET variable from ENV found!"
-        print "AWS_REGION variable from ENV found!"
-        return (os.environ["AWS_KEY"],
-                os.environ["AWS_SECRET"],
-                os.environ["AWS_REGION"])
-
-    # check if key is present as settings_file
-    try:
-        if (settings_file.AWS_ACCESS_KEY_ID and
-                settings_file.AWS_SECRET_ACCESS_KEY and
-                settings_file.AWS_REGION_NAME):
-            print "AWS_ACCESS_KEY_ID from settings_file found!"
-            print "AWS_SECRET_ACCESS_KEY from settings_file found!"
-            print "AWS_REGION_NAME from settings_file found!"
-            return (settings_file.AWS_ACCESS_KEY_ID,
-                    settings_file.AWS_SECRET_ACCESS_KEY,
-                    settings_file.AWS_REGION_NAME)
-    except Exception:
-        pass
-
-    return aws_access_key_id, aws_secret_access_key, aws_region_name
-
-
-def check_kinesis_config(aws_stream_name, aws_partition_key):
-    """
-    Function to check AWS Kinesis configuration is present and valid
-    """
-
-    # check if stream details present as input var
-    if aws_stream_name and aws_partition_key:
-        print "aws_stream_name variable from arg/flag found!"
-        print "aws_partition_key variable from arg/flag found!"
-        return aws_stream_name, aws_partition_key
-
-    # check if stream details present as env var
-    if (os.environ.get("AWS_STREAM_NAME") and
-            os.environ.get("AWS_PARTITION_KEY")):
-        print "AWS_STREAM_NAME variable from ENV found!"
-        print "AWS_PARTITION_KEY variable from ENV found!"
-        return os.environ["AWS_STREAM_NAME"], os.environ["AWS_PARTITION_KEY"]
-
-    # check if stream details present as settings_file
-    try:
-        if settings_file.AWS_STREAM_NAME and settings_file.AWS_PARTITION_KEY:
-            print "AWS_STREAM_NAME variable from settings_file found!"
-            print "AWS_PARTITION_KEY variable from settings_file found!"
-            return (settings_file.AWS_STREAM_NAME,
-                    settings_file.AWS_PARTITION_KEY)
-    except Exception:
-        pass
-
-    if aws_stream_name is None or "":
-        raise Exception("Kinesis Stream Name is invalid")
-    if aws_partition_key is None or "":
-        raise Exception("Kinesis Partition Key is invalid")
-
-    return aws_stream_name, aws_partition_key
-
-
 # main function
 def main():
     # get default arguments from user, if present
     args = parse_args()
 
-    # check domain
-    domain = check_domain(args.domain)
-
     # check ip, return ip
     ip = check_ip(args.check_url, args.ip)
 
-    # debug print ip
-    print "Public IP: {}".format(ip)
-    # debug print domain
-    print "Target Domain: {}".format(domain)
+    # check domain
+    domain = check_element("AWS_DOMAIN", args.AWS_DOMAIN)
 
     # check credentials from file/env/flags, return aws_creds
-    aws_creds = check_aws_creds(
-        args.aws_access_key_id,
-        args.aws_secret_access_key,
-        args.aws_region_name
-    )
-    # debug print creds
-    # print aws_creds[0]
-    # print aws_creds[1]
-    # print aws_creds[2]
+    aws_key = check_element("AWS_ACCESS_KEY_ID", args.AWS_ACCESS_KEY_ID)
+    aws_secret = check_element("AWS_SECRET_ACCESS_KEY", args.AWS_SECRET_ACCESS_KEY)
+    aws_region = check_element("AWS_REGION_NAME", args.AWS_REGION_NAME)
+
+    # get kinesis config from file/env/flags
+    aws_stream_name = check_element("AWS_STREAM_NAME", args.AWS_STREAM_NAME)
+    aws_partition_key = check_element("AWS_PARTITION_KEY", args.AWS_PARTITION_KEY)
 
     # setup authentication
     # http://boto3.readthedocs.io/en/latest/guide/configuration.html
@@ -259,9 +183,9 @@ def main():
     client = boto3.client(
         'kinesis',
         # Hard coded strings as credentials, not recommended.
-        aws_access_key_id=aws_creds[0],
-        aws_secret_access_key=aws_creds[1],
-        region_name=aws_creds[2]
+        aws_access_key_id=aws_key,
+        aws_secret_access_key=aws_secret,
+        region_name=aws_region
     )
 
     # debug describe shard from kinesis stream
@@ -282,28 +206,37 @@ def main():
     # print(data_message)
     # debug print UUID for user
     print "UUID of request: {}".format(data_message.get("uuid"))
+    # debug print ip
+    print "Public IP returned: {}".format(ip)
+    # debug print domain
+    print "Update target DNS record: {}".format(domain)
 
     # dump into JSON
     data_message_json = json.dumps(data_message)
     # debug print
     # print(data_message_json)
 
-    # check and collect kinesis config
-    kinesis_config = check_kinesis_config(
-        args.aws_stream_name,
-        args.aws_partition_key
-    )
+    """
+    Sample Record:
+
+    {
+        "StreamName": "lambda-update-route53-kareempoc-stream",
+        "Data": {"ip": "1.2.3.4","domain": "somedomain.com","uuid": "70e3e22a-e50e-4770-89ae-6f1d95da9b44"},
+        "PartitionKey": "shardId-000000000000"
+    }
+    """
 
     # connect to kinesis andput record on stream
     put_record_response = client.put_record(
-        StreamName=kinesis_config[0],
+        StreamName=aws_stream_name,
         Data=data_message_json,
-        PartitionKey=kinesis_config[1]
+        PartitionKey=aws_partition_key
     )
+
     # return response from kinesis
     # print success message
     if put_record_response:
-        print "Confirm sent to stream: {}".format(kinesis_config[0])
+        print "Confirm sent to stream: {}".format(aws_stream_name)
 
     return "Kinesis response: {}".format(put_record_response)
 
